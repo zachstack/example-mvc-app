ea8f1dd2bce85f67c9c82d99db041c89c265832e
alacardnightmare <zachattackhatton@gmail.com>
2021-05-15 21:03:49 -0700

    Prep for Database integration

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExampleMvcApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        // GET: api/employees
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EmployeesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
