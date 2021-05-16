43570cdd86b262a72522e64ceaa243cfc4ed1356
alacardnightmare <zachattackhatton@gmail.com>
2021-05-16 16:27:44 -0700

    Move project into its own directory

---
+++ ExampleMvcApp.Tests/TestCases/EmployeeControllerTests.cs
@@ -0,0 +1,143 @@
+using ExampleMvcApp.Controllers;
+using ExampleMvcApp.Models.Database;
+using ExampleMvcApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ExampleMvcApp.Tests.TestCases
+{
+
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        private EmployeesController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Get Config file
+            var config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.Development.json")
+                    .Build();
+
+            //Setup the necessary services
+            var services = new ServiceCollection();
+            services.AddDbContext<ExampleDbContext>(options => options.UseSqlServer(config.GetConnectionString("ExampleDb")));
+            services.AddScoped<IEmployeesRepository, EmployeesRepository>();
+            var serviceProvider = services.BuildServiceProvider();
+
+            //Get the needed repository
+            var repository = serviceProvider.GetService<IEmployeesRepository>();
+
+            _controller = new EmployeesController(repository);
+        }
+
+        [Test]
+        public async Task GetEmployees_WithNoParameters_ShouldBeOk()
+        {
+            //Execute
+            var response = await _controller.GetEmployees(null, null, null);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsTrue(response is OkObjectResult);
+
+            var result = response as OkObjectResult;
+            Assert.AreEqual((int)HttpStatusCode.OK, result.StatusCode);
+            Assert.IsTrue(result.Value is List<Employee>);
+
+            var body = (List<Employee>)result.Value;
+            Assert.IsNotEmpty(body);
+            Assert.IsTrue(body.Count == body.Select(e => e.EmployeeId).Count());
+        }
+
+        /// <summary>
+        /// Tests the endpoint GetEmployees with a variety of combinations of search parameters.
+        /// </summary>
+        /// <remarks>
+        /// Test cases assumes a static database and that there are no duplicate names.
+        /// </remarks>
+        /// <param name="name"></param>
+        /// <param name="departmentName"></param>
+        /// <param name="subDepartmentName"></param>
+        /// <returns></returns>
+        [Test]
+        [TestCase("Andrea Arkov", null, null, ExpectedResult = 4)]
+        [TestCase("Andrea Arkov", "Design Department", null, ExpectedResult = 4)]
+        [TestCase("Andrea Arkov", null, "Web Designer", ExpectedResult = 4)]
+        [TestCase("Andrea Arkov", "Design Department", "Web Designer", ExpectedResult = 4)]
+        [TestCase(null, null, "Web Designer", ExpectedResult = 4)]
+        [TestCase(null, "Design Department", "Web Designer", ExpectedResult = 4)]
+        public async Task<int> GetEmployees_WithSearchParameters_ShouldBeOneEmployee(string name, string departmentName, string subDepartmentName)
+        {
+            //Execute
+            var response = await _controller.GetEmployees(name, departmentName, subDepartmentName);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsTrue(response is OkObjectResult);
+
+            var result = response as OkObjectResult;
+            Assert.AreEqual((int)HttpStatusCode.OK, result.StatusCode);
+            Assert.IsTrue(result.Value is List<Employee>);
+
+            var body = (List<Employee>)result.Value;
+            Assert.AreEqual(1, body.Count);
+
+            return body[0].EmployeeId;
+        }
+
+        [Test]
+        [TestCase(null, "Design Department", null, ExpectedResult = 2)]
+        [TestCase(null, "Development Department", null, ExpectedResult = 2)]
+        [TestCase(null, "Account Department", null, ExpectedResult = 0)]
+        public async Task<int> GetEmployees_WithSearchParameters_ShouldBeCorrectCount(string name, string departmentName, string subDepartmentName)
+        {
+            //Execute
+            var response = await _controller.GetEmployees(name, departmentName, subDepartmentName);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsTrue(response is OkObjectResult);
+
+            var result = response as OkObjectResult;
+            Assert.AreEqual((int)HttpStatusCode.OK, result.StatusCode);
+            Assert.IsTrue(result.Value is List<Employee>);
+
+            var body = (List<Employee>)result.Value;
+            return body.Count;
+        }
+
+        [Test]
+        [TestCase(null, "Design", null)]
+        [TestCase("John", null, null)]
+        [TestCase(null, null, "Designer")]
+        [TestCase(null, "Account Department", null)]
+        [TestCase(null, null, "Video Animation")]
+        [TestCase("Oliver Queen", null, null)]
+        [TestCase("Oliver Queen", "Account Department", "Video Animation")]
+        public async Task GetEmployees_WithBadSearchParameters_ShouldReturnEmpty(string name, string departmentName, string subDepartmentName)
+        {
+            //Execute
+            var response = await _controller.GetEmployees(name, departmentName, subDepartmentName);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsTrue(response is OkObjectResult);
+
+            var result = response as OkObjectResult;
+            Assert.AreEqual((int)HttpStatusCode.OK, result.StatusCode);
+            Assert.IsTrue(result.Value is List<Employee>);
+
+            var body = (List<Employee>)result.Value;
+            Assert.IsEmpty(body);
+        }
+    }
+}
