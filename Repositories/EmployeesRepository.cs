ad85cddf70c7b91e69a5c43d1b56d5411c2388c1
alacardnightmare <zachattackhatton@gmail.com>
2021-05-16 12:34:13 -0700

    Better looking list

--- Repositories/EmployeesRepository.cs
+++ Repositories/EmployeesRepository.cs
@@ -7,19 +7,36 @@ using System.Threading.Tasks;
 
 namespace ExampleMvcApp.Repositories
 {
+    /// <summary>
+    /// Interface for an Employees Repository
+    /// </summary>
     public interface IEmployeesRepository
     {
         Task<List<Employee>> GetEmployees(string name = null, string departmentName = null, string subDepartmentName = null);
     }
 
+    /// <summary>
+    /// Repository for accessing Database Employees
+    /// </summary>
     public class EmployeesRepository : IEmployeesRepository
     {
         private readonly ExampleDbContext database;
+
         public EmployeesRepository(ExampleDbContext context)
         {
             database = context;
         }
 
+        /// <summary>
+        /// Gets a list of <see cref="Employee"/>s from the database with optional search parameters
+        /// </summary>
+        /// <remarks>
+        /// Utilizes a Stored Procedure
+        /// </remarks>
+        /// <param name="name"></param>
+        /// <param name="departmentName"></param>
+        /// <param name="subDepartmentName"></param>
+        /// <returns>A list of <see cref="Employee"/>s</returns>
         public async Task<List<Employee>> GetEmployees(string name = null, string departmentName = null, string subDepartmentName = null)
         {
             //Trim and format string and convert to empty string in cases of null
